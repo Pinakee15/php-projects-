7dda115b625e1c610216d4d926b5f1a255c219ab
pinakee15 <iampinakee2098@gmail.com>
2020-11-26 20:29:42 +0530

    Added initial files

---
+++ templates/header.php
@@ -0,0 +1,14 @@
+<head>
+    <title>Pizza Delievery</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link rel="stylesheet" type="text/css" href="css/index.css">
+</head>
+    <body class = "grey lighten-4">
+    <nav class = "white z-depth-0">
+        <div class="container">
+            <a href="#" class = "brand-logo brand-text">Pizza Bay</a>
+            <ul id ="nav-mobile" class = "right hide-on-small-and-down">
+                <li><a href="#" class ="btn brand z-depth-0">Add a Pizza</a></li>
+            </ul>
+        </div>    
+    </nav>
